9dd168b0a4fa9ce846eb03c968b9e26fddb1b327
NN-Guillaume <35535667+NN-Guillaume@users.noreply.github.com>
2019-03-05 18:16:28 +0100

    Add files via upload

---
+++ constantes.py
@@ -0,0 +1,33 @@
+""" MacGyver's game constants """
+
+# windows parameters
+nbr_side_sprite = 15
+sprite_size = 30
+window_side = nbr_side_sprite * sprite_size
+
+# customisation of the window (?)
+window_title = "Take MacGyver out of here !"
+icon_picture = "ressources/MacGyver.png"
+
+# All pictures of the game
+
+# characters
+pic_mg = "ressources/MacGyver.png"
+pic_gd = "ressources/Gardien.png"
+
+# collectables
+pic_nid = "ressources/aiguille.png"         # the niddle
+pic_eth = "ressources/ether.png"            # the Ether bottle
+pic_tub = "ressources/tube_plastique.png"   # the plastic tube
+
+# when the three collectables are reunited
+pic_inj = "ressources/seringue.png"
+
+# graphic elements of the level
+pic_bg = "ressources/background.jpg"       # Background picture !
+pic_start = "ressources/start.png"         # Starting point !
+pic_w1 = "ressources/sand.png"             # Zone 1 Wall picture !
+pic_w2 = "ressources/grass.png"            # Zone 2 Wall picture !           
+pic_w3 = "ressources/water.png"            # Zone 3 Wall picture !           
+pic_w4 = "ressources/brick.png"            # Zone 4 Wall picture !
+pic_end = "ressources/end.png"             # ending case !           
